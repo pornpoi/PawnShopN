db9f00913547fb31fe378a199b63747d6f44839a
nookple <nookple@gmail.com>
2017-02-28 16:26:13 +0700

    yyyy

--- AuctionArea/CommitteeController.aspx.vb
+++ AuctionArea/CommitteeController.aspx.vb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿Partial Class AuctionArea_CommitteeController
     Inherits System.Web.UI.Page
     Public Shared eventId As String = "1"
@@ -14,21 +13,3 @@
         End If
     End Sub
 End Class
-=======
-﻿
-Partial Class AuctionArea_CommitteeController
-    Inherits System.Web.UI.Page
-    Public Shared eventId As String = "1"
-    Public Shared EventNo As String = ""
-    Public Shared DateEventStart As String = ""
-    Public Shared GroupName As String = ""
-    Private Sub AuctionArea_CommitteeController_Load(sender As Object, e As EventArgs) Handles Me.Load
-        If (Request.QueryString("EventID") IsNot Nothing) Then
-            eventId = "'" & Request.QueryString("EventID").ToString & "'"
-            EventNo = Request.QueryString("EventNo").ToString
-            DateEventStart = Request.QueryString("DateEventStart").ToString
-            GroupName = Request.QueryString("GroupName").ToString
-        End If
-    End Sub
-End Class
->>>>>>> origin/master
